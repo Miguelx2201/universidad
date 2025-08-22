0b3d4a30ef436734150404a9939ea2cbc6ada634
Juan Miguel Gutierrez V. <juanm.gutierrezv@uqvirtual.edu.co>
2025-08-22 15:45:15 -0500

    Commit inicial del proyecto

---
+++ src/main/java/model/Course.java
@@ -0,0 +1,100 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Course {
+    private String name;
+    private String code;
+    private String description;
+    private int credits;
+    private String semester;
+    private ArrayList<Student> listStudents;
+
+    public Course(String name, String code, String description, int credits, String semester){
+        this.name = name;
+        this.code = code;
+        this.description = description;
+        this.credits = credits;
+        this.semester = semester;
+        this.listStudents = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public ArrayList<Student> getListStudents() {
+        return listStudents;
+    }
+
+    public void setListStudents(ArrayList<Student> listStudents) {
+        this.listStudents = listStudents;
+    }
+    public String addStudent(Student student){
+        String result = "";
+        Student foundStudent = searchStudent(student.getId());
+        if(foundStudent == null){
+            listStudents.add(student);
+            result = "Student added successfully";
+        } else {
+            result = "Student already exists";
+        }
+        return result;
+    }
+    public Student searchStudent(String id){
+        Student foundStudent = null;
+        for(Student student : listStudents){
+            if(student.getId().equals(id)){
+                foundStudent = student;
+            }
+        }
+        return foundStudent;
+    }
+    public String removeStudent(Student student) {
+        String result = "";
+        Student foundStudent = searchStudent(student.getId());
+        if(foundStudent != null) {
+            listStudents.remove(foundStudent);
+            result = "La mascota ha sido eliminada exitosamente.";
+        } else {
+            result = "La mascota no ha sido encontrada en el sistema.";
+        }
+        return result;
+    }
+}
---
+++ src/main/java/model/Student.java
@@ -0,0 +1,58 @@
+package model;
+
+public class Student {
+    private String id;
+    private String name;
+    private String lastName;
+    private byte age;
+    private String email;
+    private String telephone;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
